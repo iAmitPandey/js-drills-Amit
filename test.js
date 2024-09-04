bddf1efbf784f521555c20bdd0fc342ab4c0cba2
Amit Pandey <amitpandey21599@gmail.com>
2024-09-04 11:56:45 +0530

    Added the test cases for second problem

--- test.js
+++ test.js
@@ -1,5 +1,5 @@
 import arrayOfObjects from './persons.js';
-import {emailAddressOfAll} from './solutions.js'
+import {emailAddressOfAll, hobbyInAge} from './solutions.js'
 
 
 //    function that accesses and returns the email addresses of all individuals.
@@ -11,3 +11,11 @@ for(let i = 0; i<arrayOfObjects.length; i++){
     emailAddressOfAll({});
 }
 
+// function that retrieves and prints the hobbies of individuals with a specific age, say 30 years old.
+
+for(let i = 0; i<arrayOfObjects.length; i++){
+    hobbyInAge(arrayOfObjects[i], arrayOfObjects[i].age);
+    hobbyInAge(undefined, 0);
+    hobbyInAge(null, null);
+    hobbyInAge({}, 0);
+}
